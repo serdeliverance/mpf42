432a202bfc3b597af4f2836795671cf25b3c35a9
Sergio Cano <canosergio90@gmail.com>
2022-12-24 11:02:24 +0100

    initial commit
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

---
+++ src/main/java/io/github/sd3v/mpf/MpfApplication.java
@@ -0,0 +1,13 @@
+package io.github.sd3v.mpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sd3v/mpf/MpfApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sd3v.mpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
